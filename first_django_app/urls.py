5bf0329f7d5d02279b8d1cbd68dd26d4a395c827
Nina Villarreal <ninavillarreal82@yahoo.com>
2021-05-01 17:35:29 -0500

    update route names

--- first_django_app/urls.py
+++ first_django_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path     
 from . import views
+
+
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new),
-    path('create', views.create),
-    path('<int:number>', views.show),
-    path('<int:number>/edit', views.edit),
-    path('<int:number>/delete', views.destroy),
+    path('blogs', views.index),
+    path('blogs/new', views.new),
+    path('blogs/create', views.create),
+    path('blogs/<int:number>', views.show),
+    path('blogs/<int:number>/edit', views.edit),
+    path('blogs/<int:number>/delete', views.destroy),
 ]
