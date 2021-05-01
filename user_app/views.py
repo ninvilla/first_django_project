7d3384494e43ddaea5b1977d62daca656bd70ef0
Nina Villarreal <ninavillarreal82@yahoo.com>
2021-05-01 17:41:13 -0500

    create user app

---
+++ user_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def register(request):
+    return HttpResponse("placeholder for users to create a new user record")
+
+def login(request):
+    return HttpResponse("placeholder for users to log in")
+
+def new(request):
+    return redirect('/')
+
+def users(request):
+    return HttpResponse("placeholder to later display all the list of users")
