8440a9f6353d11b399eab469b4b64ca1ec18c61d
Nina Villarreal <ninavillarreal82@yahoo.com>
2021-05-01 17:36:27 -0500

    update redirect routes

--- first_django_app/views.py
+++ first_django_app/views.py
@@ -1,5 +1,7 @@
 
 from django.shortcuts import render, HttpResponse, redirect
+
+
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
     
@@ -7,7 +9,7 @@ def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
 
 def create(request):
-    return redirect("/")
+    return redirect("/blogs/blogs")
 
 def show(request, number):
     return HttpResponse(f"placeholder to display blog number: {number}")
@@ -16,4 +18,4 @@ def edit(request, number):
     return HttpResponse(f"placeholder to edit blog number: {number}")
 
 def destroy(request, number):
-    return redirect("/")
+    return redirect("/blogs/blogs")
