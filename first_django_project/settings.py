1090bbf0579e72c362bb11111619f4e8a21bf4e5
Nina Villarreal <ninavillarreal82@yahoo.com>
2021-05-01 17:37:47 -0500

    add survey and user apps

--- first_django_project/settings.py
+++ first_django_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'first_django_app',
+    'survey_app',
+    'user_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- first_django_project/urls.py
+++ first_django_project/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', include('first_django_app.urls')),
+    path('blogs/', include('first_django_app.urls')),
+    path('surveys/', include('survey_app.urls')),
+    path('', include('user_app.urls')),
 ]
